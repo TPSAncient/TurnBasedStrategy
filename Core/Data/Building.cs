002680879c712cce1ae0d80cad23269534547228
parnh <Herki.Parn@advancedcomputersoftware.com>
2015-08-27 15:35:53 +0100

    Added building data class

---
+++ Core/Data/Building.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data
+{
+    public class Building
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // Building Type
+        // Building requirment
+        // Building maintainence
+    }
+}

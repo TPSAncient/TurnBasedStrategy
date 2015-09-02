38686885f074767dcb5f79493562e44287a47b40
unknown <herki77@gmail.com>
2015-09-02 10:29:13 +0100

    Added server and client project for testing code

--- NClient/Program.cs
+++ NClient/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,19 @@ namespace NClient
     {
         static void Main(string[] args)
         {
+            var client = new UdpClient();
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000); // endpoint where server is listening
+            client.Connect(ep);
+
+            // send data
+            client.Send(new byte[] { 1, 2, 3, 4, 5 }, 5);
+
+            // then receive data
+            var receivedData = client.Receive(ref ep);
+
+            Console.Write("receive data from " + ep.ToString());
+
+            Console.Read();
         }
     }
 }

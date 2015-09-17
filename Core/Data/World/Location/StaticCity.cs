6a5a79192a98a27842950f756b29c47c36469a4a
unknown <parnh@LAPTOP-HERKI.adastra.co.uk>
2015-09-17 14:10:00 +0100

    Added example data province, extended province system

--- Core/Data/World/Location/StaticCity.cs
+++ Core/Data/World/Location/StaticCity.cs
@@ -5,9 +5,12 @@
         // General
         public int Id { get; set; }
         public string Name { get; set; }
+        public LocationType LocationType { get; set; }
 
         // Spesific
         public int PopulationCount { get; set; }
+        public int PopulationBirthRate { get; set; }
+        public int PopulationImmigrationRate { get; set; }
         // Building List
         
     }
--- Core/Data/World/StaticProvince.cs
+++ Core/Data/World/StaticProvince.cs
@@ -1,9 +1,16 @@
-﻿namespace Core.Data.World
+﻿using System.Collections.Generic;
+using Core.Data.World.Location;
+
+namespace Core.Data.World
 {
     public class StaticProvince 
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public List<ILocation> Locations { get; set; } 
+        
         // List of Buildable Spaces
+
     }
 }

268f637992e96f793e2ab05de806df61996d1f38
herki18 <herki77@gmail.com>
2015-10-13 16:32:15 +0100

    Changes

---
+++ Core/System/UnityMonoBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.System
+{
+    public abstract class UnityMonoBehaviour
+    {
+        public virtual void Awake() { }
+        public virtual void Start() { }
+        public virtual void Update() { }
+    }
+}

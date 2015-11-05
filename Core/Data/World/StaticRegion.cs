c9714208e3055117d47a09096b04ef29e097108b
herki18 <herki77@gmail.com>
2015-11-05 16:16:28 +0000

    changes

---
+++ Core/Data/World/StaticRegion.cs
@@ -0,0 +1,17 @@
+﻿using Core.Data.World.Location;
+
+namespace Core.Data.World
+{
+    public class StaticRegion
+    {
+        // Only one may be in region
+        public StaticCity City { get; set; }
+        public StaticSettlement Settlement { get; set; }
+        
+
+        public StaticFarm Farm { get; set; }
+        public StaticIndustry Industry { get; set; }
+        public StaticPort Port { get; set; }
+        
+    }
+}

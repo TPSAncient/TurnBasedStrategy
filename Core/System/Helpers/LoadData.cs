db7ab1c50471806fdbc0aedf87c326f22ca9d298
herki18 <herki77@gmail.com>
2015-11-10 16:54:25 +0000

    Refactor

---
+++ Core/System/Helpers/LoadData.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.System.Helpers
+{
+    public static class LoadData
+    {
+        public static T Load<T>(string fileName)
+        {
+            return JsonData.LoadJson<T>(fileName);
+        }
+    }
+}

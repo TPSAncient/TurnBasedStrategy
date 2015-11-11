ad6575ab4cd47a60ed01022eae44b9193a1f45ab
herki18 <herki77@gmail.com>
2015-11-11 11:31:43 +0000

    Deleted NServer

---
+++ Core/Data/Army/StaticArmy.cs
@@ -0,0 +1,13 @@
+﻿using Core.Data.Common;
+
+namespace Core.Data.Army
+{
+    public class StaticArmy : IData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string TagName { get; set; }
+        public DataType DataType { get; set; }
+
+    }
+}
